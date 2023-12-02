2ff4bd7c14357b24df1086650fcd1bc9b0a890a3
Chris Hannam <ch@chrishannam.co.uk>
2023-12-02 10:36:23 +0000

    Add some more plugs

--- pluggy/main.py
+++ pluggy/main.py
@@ -18,68 +18,99 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+PLUGS = {
+    'driving rig':
+        {
+            'ip_address': '192.168.0.51',
+            'location': 'office',
+        },
+    'tv':
+        {
+            'ip_address': '192.168.0.52',
+            'location': 'front room',
+        },
+    'heater':
+        {
+            'ip_address': '192.168.0.57',
+            'location': 'office',
+        },
+    # 'heating rail': '192.168.0.59',
+    # 'clothes dryer':
+    #     {
+    #         'ip_address': '192.168.0.59',
+    #         'location': 'office',
+    #     },
+    'desk':
+        {
+            'ip_address': '192.168.0.60',
+            'location': 'office',
+        },
+}
 
 
 async def main():
     client = ApiClient(TAPO_USERNAME, TAPO_PASSWORD)
-    device = await client.p110("192.168.0.57")
-    client = influxdb_client.InfluxDBClient(url=URL, token=TOKEN, org=ORG)
-    write_api = client.write_api(write_options=SYNCHRONOUS)
 
-    device_info = await device.get_device_info()
-    print(f"Device info: {device_info.to_dict()}")
+    for name, details in PLUGS.items():
+        print(f"Starting {name} -> {details['ip_address']}")
+        device = await client.p110(details['ip_address'])
 
-    device_usage = await device.get_device_usage()
-    print(f"Device usage: {device_usage.to_dict()}")
+        influx_client = influxdb_client.InfluxDBClient(url=URL, token=TOKEN, org=ORG)
+        write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 
-    current_power = await device.get_current_power()
-    print(f"Current power: {current_power.to_dict()}")
+        device_info = await device.get_device_info()
+        #print(f"Device info: {device_info.to_dict()}")
 
-    energy_usage = await device.get_energy_usage()
-    print(f"Energy usage: {energy_usage.to_dict()}")
+        device_usage = await device.get_device_usage()
+        #print(f"Device usage: {device_usage.to_dict()}")
 
-    point = (
-        Point("tapo")
-        .tag("location", "office")
-        .tag("type", "heater")
-        .field("on", 1 if device_info.device_on else 0)
-    )
-    write_api.write(bucket=BUCKET, org=ORG, record=point)
+        current_power = await device.get_current_power()
+        #print(f"Current power: {current_power.to_dict()}")
 
-    for date_range in ["past30", "past7", "today"]:
-        point = (
-            Point("tapo")
-            .tag("location", "office")
-            .tag("type", "heater")
-            .field(date_range, device_usage.to_dict()["power_usage"][date_range])
-        )
-        write_api.write(bucket=BUCKET, org=ORG, record=point)
-
-    for date_range in ["past30", "past7", "today"]:
-        point = (
-            Point("tapo")
-            .tag("location", "office")
-            .tag("type", "heater")
-            .field(date_range, device_usage.to_dict()["time_usage"][date_range])
-        )
-        write_api.write(bucket=BUCKET, org=ORG, record=point)
-
-
-    for reading, value in energy_usage.to_dict().items():
-        print(f'{reading} -> {value}')
-
-        if reading == "local_time":
-            continue
+        energy_usage = await device.get_energy_usage()
+        #print(f"Energy usage: {energy_usage.to_dict()}")
 
         point = (
             Point("tapo")
             .tag("location", "office")
-            .tag("type", "heater")
-            .field(reading, value)
+            .tag("name", name)
+            .field("on", 1 if device_info.device_on else 0)
         )
         write_api.write(bucket=BUCKET, org=ORG, record=point)
 
-    print("Complete!")
+        for date_range in ["past30", "past7", "today"]:
+            point = (
+                Point("tapo")
+                .tag("location", details['location'])
+                .tag("name", name)
+                .field(date_range, device_usage.to_dict()["power_usage"][date_range])
+            )
+            write_api.write(bucket=BUCKET, org=ORG, record=point)
+
+        for date_range in ["past30", "past7", "today"]:
+            point = (
+                Point("tapo")
+                .tag("location", details['location'])
+                .tag("name", name)
+                .field(date_range, device_usage.to_dict()["time_usage"][date_range])
+            )
+            write_api.write(bucket=BUCKET, org=ORG, record=point)
+
+        for reading, value in energy_usage.to_dict().items():
+            #print(f'{reading} -> {value}')
+
+            if reading == "local_time":
+                continue
+
+            point = (
+                Point("tapo")
+                .tag("location", details['location'])
+                .tag("name", name)
+                .field(reading, value)
+            )
+            write_api.write(bucket=BUCKET, org=ORG, record=point)
+
+        print(f"Completed {name} -> {details}!")
 
 
 if __name__ == '__main__':
